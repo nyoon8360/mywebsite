85773e8c3efd8aeaa73dd27bb1664000a1dc0a18
Nicholas Yoon <nyoon8360@gmail.com>
2023-09-25 14:02:51 -0400

    added the react-awesome-reveal library for custom animations when components enter viewport

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,39 @@
 import Image from 'next/image';
 import Layout from '../components/layout';
 import styles from '../styles/index.module.css';
+import {Reveal} from 'react-awesome-reveal';
+import { keyframes } from '@emotion/react';
 
 export default function HomePage() {
+
+    const fadeUpAnimation = keyframes`
+        from {
+            opacity: 0;
+            transform: translateY(5px);
+        }
+
+        to {
+            opacity: 1;
+            transform: translateY(0);
+        }
+    `;
 
     return (
         <Layout>
             <div className={styles.profilePicContainer}>
             <Image className={styles.profilePic} src='/images/profile_pic_rain_car.jpg' height={250} width={250} alt='My Pic'/>
             </div>
-            <p>Hey I'm Nick, a {calc_age(new Date(1998, 7, 27))} years old software developer, tattoo enthusiast, and massive music fan!</p>
-            <p>I made this website to house my interests, show off some of my personal projects I've enjoyed working 
+            <Reveal keyframes={fadeUpAnimation} delay={500}>
+                <p>Hey I'm Nick, a {calc_age(new Date(1998, 7, 27))} years old software developer, tattoo enthusiast, and massive music fan!</p>
+
+                <p>I made this website to house my interests, show off some of my personal projects I've enjoyed working 
                 on, and to learn the Next.JS framework through hands-on experience.</p>
-            <p></p>
-            <p>Intro, Age, Bday, College, Degree, Interests</p>
+
+                <p></p>
+
+                <p>Intro, Age, Bday, College, Degree, Interests</p>
+            </Reveal>
+            
         </Layout>
     )
 }
