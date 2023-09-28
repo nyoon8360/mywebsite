c2d362501fe99549184f633178bb5c7daf46b812
Nicholas Yoon <nyoon8360@gmail.com>
2023-09-28 12:14:07 -0400

    implemented collapsible bar icon toggling and class toggling

--- pages/index.js
+++ pages/index.js
@@ -21,17 +21,18 @@ export default function HomePage() {
     return (
         <Layout>
             <div className={styles.profilePicContainer}>
-            <Image className={styles.profilePic} src='/images/profile_pic_rain_car.jpg' height={250} width={250} alt='My Pic'/>
+                <Image className={styles.profilePic} src='/images/profile_pic_rain_car.jpg' height={250} width={250} alt='My Pic'/>
             </div>
+            <hr className={styles.divider}/>
             <Reveal keyframes={fadeUpAnimation} delay={500}>
-                <p>Hey I'm Nick, a {calc_age(new Date(1998, 7, 27))} years old software developer, tattoo enthusiast, and massive music fan!</p>
+                <p className={styles.textBodyContent}>Hey I'm Nick, a {calc_age(new Date(1998, 7, 27))} years old software developer, tattoo enthusiast, and massive music fan!</p>
 
-                <p>I made this website to house my interests, show off some of my personal projects I've enjoyed working 
+                <p className={styles.textBodyContent}>I made this website to house my interests, show off some of my personal projects I've enjoyed working 
                 on, and to learn the Next.JS framework through hands-on experience.</p>
 
-                <p></p>
+                <p className={styles.textBodyContent}></p>
 
-                <p>Intro, Age, Bday, College, Degree, Interests</p>
+                <p className={styles.textBodyContent}>Intro, Age, Bday, College, Degree, Interests</p>
             </Reveal>
             
         </Layout>
--- pages/music-likes.js
+++ pages/music-likes.js
@@ -1,11 +1,55 @@
 import Layout from '../components/layout';
+import styles from '../styles/music-likes.module.css';
+import Image from 'next/image';
+import {Reveal} from 'react-awesome-reveal';
+import { keyframes } from '@emotion/react';
 
 export default function MusicLikes() {
 
+    const fadeUpAnimation = keyframes`
+        from {
+            opacity: 0;
+            transform: translateY(5px);
+        }
+
+        to {
+            opacity: 1;
+            transform: translateY(0);
+        }
+    `;
+
     return (
         <Layout>
-            <p>Lorem Ipsum</p>
-            <p>music page</p>
+            <h3 className={styles.category}>Favorite Artist</h3>
+
+            <Reveal keyframes={fadeUpAnimation} delay={500}>
+                <iframe style={{borderRadius: '12px'}} 
+                src="https://open.spotify.com/embed/artist/3pc0bOVB5whxmD50W79wwO?utm_source=generator" 
+                width="100%" height="152" frameBorder="0" allowFullScreen="" 
+                allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" 
+                loading="lazy"></iframe>
+            </Reveal>
+
+            <hr className={styles.divider}/>
+
+            <h3 className={styles.category}>Favorite Genres</h3>
+
+            <Reveal keyframes={fadeUpAnimation} delay={500}>
+                <button className={styles.collapsible} onClick={toggleCollapsible}>
+                    <Image className={styles.collapsibleSvg} src='/svgs/down-arrow.svg' height={50} width={50} alt='arrow'/>
+                </button>
+            </Reveal>
+
+            <hr className={styles.divider}/>
+            
         </Layout>
     )
+}
+
+function toggleCollapsible(event) {
+    let collapsibleElement = event.target;
+    
+    collapsibleElement.classList.toggle('.activeCollapsible');
+
+    collapsibleElement.children[0].src = collapsibleElement.children[0].getAttribute('src') == '/svgs/down-arrow.svg' ? '/svgs/up-arrow.svg' : '/svgs/down-arrow.svg';
 }
