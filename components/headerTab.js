eb4ecf9d8f03b54f3bf8f4498df959ac4ec6c027
Nicholas Yoon <nyoon8360@gmail.com>
2023-11-13 21:40:45 -0500

    remade headerTab component to have multiple different states it can be constructed in
    made falling tab also have wave animation that fills the tab as it drops

--- components/headerTab.js
+++ components/headerTab.js
@@ -1,33 +1,55 @@
 import styles from './headerTab.module.css';
 
-export default function headerTab({tabId, underlineColor, destinationPath, currentPath, onClickEvent, waveSvgColor, children}) {
+export default function headerTab({tabState, underlineColor, destinationPath, onClickEvent, children}) {
     //throw error if component has child that is not just inner text
     if (typeof children != 'string') throw new Error('Children must only be inner text.');
 
-    //tab style definition of a tab that is active as the current page
-    let tabStyle = {
-        backgroundColor: '#78bfe8',
-        cursor: 'default',
-        textDecorationColor: underlineColor
+    let backgroundSvgUri = `url("data:image/svg+xml,%3Csvg preserveAspectRatio='none' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 1440 585'%3E%3Cpath fill='%23${underlineColor.substring(1)}' fill-opacity='1' d='M 0 32 L 24 48 C 48 64 96 96 144 138.7 C 192 181 240 235 288 229.3 C 336 224 384 160 432 160 C 480 160 528 224 576 218.7 C 624 213 672 139 720 90.7 C 768 43 816 21 864 42.7 C 912 64 960 128 1008 165.3 C 1056 203 1104 213 1152 192 C 1200 171 1248 117 1296 85.3 C 1344 53 1392 43 1416 37.3 L 1440 32 L 1440 585 L 1416 585 C 1392 585 1344 585 1296 585 C 1248 585 1200 585 1152 585 C 1104 585 1056 585 1008 585 C 960 585 912 585 864 585 C 816 585 768 585 720 585 C 672 585 624 585 576 585 C 528 585 480 585 432 585 C 384 585 336 585 288 585 C 240 585 192 585 144 585 C 48.9 585 158.9 585 24 585 L 0 585 L 0 32 Z'%3E%3C/path%3E%3C/svg%3E")`;
+
+    //depending on tabState parameter, return corresponding tab element
+    switch (tabState) {
+        //tab that is selected as the current page
+        case 'selected':
+            return (
+                <div className={styles.mainContainer}>
+                    <div 
+                        style={{textDecorationColor: underlineColor, backgroundColor: '#78bfe8', cursor: 'default'}}
+                        className={styles.tab}>
+                        {children}
+                    </div>
+                </div>
+            )
+
+        //normal clickable tab element
+        case 'normal':
+            return (
+                <div className={styles.mainContainer}>
+                    <div className={styles.wave} style={{backgroundImage: backgroundSvgUri}}></div>
+                    <div 
+                        style={{textDecorationColor: underlineColor}}
+                        page-route={destinationPath} 
+                        className={styles.tab} 
+                        onClick={onClickEvent}
+                        onMouseEnter={tabMouseEnterEvent}
+                        onMouseLeave={tabMouseLeaveEvent}>
+                        {children}
+                    </div>
+                </div>
+            )
+        
+        //disabled tab with normal styling but can not be clicked or interacted with
+        case 'disabled':
+            return (
+                <div className={styles.mainContainer}>
+                    <div 
+                        style={{textDecorationColor: underlineColor, cursor: 'default'}}
+                        page-route={destinationPath} 
+                        className={styles.tab}>
+                        {children}
+                    </div>
+                </div>
+            )
     }
-
-    let backgroundSvgUri = `url("data:image/svg+xml,%3Csvg preserveAspectRatio='none' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 1440 585'%3E%3Cpath fill='%23${waveSvgColor}' fill-opacity='1' d='M 0 32 L 24 48 C 48 64 96 96 144 138.7 C 192 181 240 235 288 229.3 C 336 224 384 160 432 160 C 480 160 528 224 576 218.7 C 624 213 672 139 720 90.7 C 768 43 816 21 864 42.7 C 912 64 960 128 1008 165.3 C 1056 203 1104 213 1152 192 C 1200 171 1248 117 1296 85.3 C 1344 53 1392 43 1416 37.3 L 1440 32 L 1440 585 L 1416 585 C 1392 585 1344 585 1296 585 C 1248 585 1200 585 1152 585 C 1104 585 1056 585 1008 585 C 960 585 912 585 864 585 C 816 585 768 585 720 585 C 672 585 624 585 576 585 C 528 585 480 585 432 585 C 384 585 336 585 288 585 C 240 585 192 585 144 585 C 48.9 585 158.9 585 24 585 L 0 585 L 0 32 Z'%3E%3C/path%3E%3C/svg%3E")`;
-
-    return (
-        <div className={styles.mainContainer}>
-            <div className={styles.wave} style={{backgroundImage: backgroundSvgUri}}></div>
-            <div 
-                id={tabId} 
-                style={currentPath == destinationPath ? tabStyle : {textDecorationColor: underlineColor}}
-                page-route={destinationPath} 
-                className={styles.tab} 
-                onClick={currentPath == destinationPath ? null : onClickEvent}
-                onMouseEnter={tabMouseEnterEvent}
-                onMouseLeave={tabMouseLeaveEvent}>
-                {children}
-            </div>
-        </div>
-    )
 }
 
 function tabMouseEnterEvent(event) {
--- components/layout.js
+++ components/layout.js
@@ -3,7 +3,6 @@ import styles from './layout.module.css';
 import { useRouter } from 'next/router';
 import { useTransitionContext } from '../context/transition';
 
-var blockFadeIn = false;
 var fadeColor = 'transparent';
 var transitionContainerOnAnimEnd;
 
@@ -13,23 +12,22 @@ export default function Layout({ children }) {
     const [tColor, setTColor] = useTransitionContext();
 
     //function creating and playing expanding circle transition animation
-    function transition(clickedTabEle, transitionColor) {
+    function transition(clickedTabEle, clickedTabEleXCoord, clickedTabEleWidth, transitionColor) {
         //get styles and coords from passed tab element
-        let tabXcoord = clickedTabEle.getBoundingClientRect().left;
-        let tabComputedStyle = getComputedStyle(clickedTabEle);
-
-        blockFadeIn = true;
+        console.log(clickedTabEleWidth);
     
         //get ripple transition div
         let rippleTransitionDiv = document.getElementById('rippleTransition');
         let rippleTransitionRingDiv = document.getElementById('rippleTransitionRing');
 
-        rippleTransitionDiv.style.left = `${(Math.max(0, tabXcoord) + (parseFloat(tabComputedStyle.width.slice(0, -2))/2))}px`;
+        console.log(`${(Math.max(0, clickedTabEleXCoord) + (parseFloat(clickedTabEleWidth.slice(0, -2))/2))}px`);
+
+        rippleTransitionDiv.style.left = `${(Math.max(0, clickedTabEleXCoord) + (parseFloat(clickedTabEleWidth.slice(0, -2))/2))}px`;
         rippleTransitionDiv.style.width = '9000px';
         rippleTransitionDiv.style.height = '4500px';
         rippleTransitionDiv.style.background = `radial-gradient(circle at 50% 100%,${transitionColor} 20%, rgba(162, 212, 240, 0) 70%)`
 
-        rippleTransitionRingDiv.style.left = `${(Math.max(0, tabXcoord) + (parseFloat(tabComputedStyle.width.slice(0, -2))/2))}px`;
+        rippleTransitionRingDiv.style.left = `${(Math.max(0, clickedTabEleXCoord) + (parseFloat(clickedTabEleWidth.slice(0, -2))/2))}px`;
         rippleTransitionRingDiv.style.width = '9000px';
         rippleTransitionRingDiv.style.height = '4500px';
         rippleTransitionRingDiv.style.background = `radial-gradient(circle at 50% 100%, rgba(162, 212, 240, 0) 65%, ${transitionColor} 70%)`
