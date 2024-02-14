21bd9a4a2351ef7b11c3685c23b79baec33ff184
Nicholas Yoon <nyoon8360@gmail.com>
2024-02-13 21:03:30 -0500

    fixed collapsible component content background color
    added embed for linkedin profile on socials page

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,7 @@ import { TransitionProvider } from '../context/transition'
 export default function MyApp({ Component, pageProps }) {
     return (
         <TransitionProvider>
+            <script src="https://platform.linkedin.com/badges/js/profile.js" async defer type="text/javascript"></script>
             <Component {...pageProps} />
         </TransitionProvider>
     )
--- pages/socials.js
+++ pages/socials.js
@@ -1,15 +1,30 @@
+import { useEffect } from 'react';
 import Layout from '../components/layout';
 import styles from '../styles/socials.module.css';
-import { InstagramEmbed } from 'react-social-media-embed';
+import Script from 'next/script';
 
 export default function Socials() {
-    
+
+    //Due to behavior of navigation in Next.Js, need to employ useEffect to load 3rd party script successfully
+    //before attempting to render embed components
+    useEffect(() => {
+        const script = document.createElement("script");
+
+        script.src = 'https://platform.linkedin.com/badges/js/profile.js';
+        script.async = true;
+
+        document.body.appendChild(script);
+
+        return () => {
+            document.body.removeChild(script);
+        }
+    });
 
     return (
         <Layout>
-            <p>Lorem Ipsum</p>
-            <p>socials page</p>
-        
+            <h3 className={styles.socialTitle} style={{marginTop: '30px'}}>LinkedIn</h3>
+            <div class="badge-base LI-profile-badge" data-locale="en_US" data-size="large" data-theme="dark" data-type="HORIZONTAL" data-vanity="nyoon8360" data-version="v1"><a class="badge-base__link LI-simple-link" href="https://www.linkedin.com/in/nyoon8360?trk=profile-badge"></a></div>
+            <hr className={styles.divider}></hr>
         </Layout>
     )
 }
