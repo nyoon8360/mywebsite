d8fb0113034de5f4299fecb9ea7cf4d6f1d1a677
Nicholas Yoon <nyoon8360@gmail.com>
2024-05-19 15:05:59 -0400

    properly centered collapsible text with reactive font sizing

--- components/headerTab.js
+++ components/headerTab.js
@@ -13,38 +13,6 @@ const cloudCirclesTransformList = [
     "translate(14px, 8px)"
 ]
 
-export default function headerTab({tabState, underlineColor, destinationPath, onClickEvent, children}) {
-    //throw error if component has child that is not just inner text
-    if (typeof children != 'string') throw new Error('Children must only be inner text.');
-
-    const router = useRouter();
-
-    function mouseClickEvent() {
-        router.push(destinationPath);
-    }
-
-    return (
-        <div className={styles.mainContainer} onMouseEnter={tabMouseEnterEvent} onMouseLeave={tabMouseLeaveEvent} onClick={mouseClickEvent}>
-            <div 
-                style={{textDecorationColor: underlineColor}}
-                page-route={destinationPath} 
-                className={styles.tab} 
-                onClick={onClickEvent}>
-                {children}
-            </div>
-            <div className={styles.cloudContainer}>
-                <div className={`${styles.cloudCircle} ${styles.cloudCirclePath1}`}/>
-                <div className={`${styles.cloudCircle} ${styles.cloudCirclePath2}`}/>
-                <div className={`${styles.cloudCircle} ${styles.cloudCirclePath3}`}/>
-                <div className={`${styles.cloudCircle} ${styles.cloudCirclePath4}`}/>
-                <div className={`${styles.cloudCircle} ${styles.cloudCirclePath5}`}/>
-                <div className={`${styles.cloudCircle} ${styles.cloudCirclePath6}`}/>
-            </div>
-            <Image className={styles.rainDrop} src={raindrop} height={30} width={30} alt='raindrop' />
-        </div>
-    )
-}
-
 function tabMouseEnterEvent(event) {
     let cloudCirclesCollection = event.target.children[1].children;
 
@@ -64,4 +32,42 @@ function tabMouseLeaveEvent(event) {
 
         cloudCirclesCollection.item(i).style.transform = null;
     }
+}
+
+export default function headerTab({tabState, underlineColor, destinationPath, onClickEvent, children}) {
+    //throw error if component has child that is not just inner text
+    if (typeof children != 'string') throw new Error('Children must only be inner text.');
+
+    const router = useRouter();
+
+    function handleTabClicked(event) {
+        event.currentTarget.children[2].style.top = '100vh';
+        event.currentTarget.children[2].style.visibility = 'visible';
+
+    }
+
+    function handleRaindropTransitionEnd() {
+        router.push(destinationPath);
+    }
+
+    return (
+        <div className={styles.mainContainer} onMouseEnter={tabMouseEnterEvent} onMouseLeave={tabMouseLeaveEvent} onClick={handleTabClicked}>
+            <div 
+                style={{textDecorationColor: underlineColor}}
+                page-route={destinationPath} 
+                className={styles.tab} 
+                onClick={onClickEvent}>
+                {children}
+            </div>
+            <div className={styles.cloudContainer}>
+                <div className={`${styles.cloudCircle} ${styles.cloudCirclePath1}`}/>
+                <div className={`${styles.cloudCircle} ${styles.cloudCirclePath2}`}/>
+                <div className={`${styles.cloudCircle} ${styles.cloudCirclePath3}`}/>
+                <div className={`${styles.cloudCircle} ${styles.cloudCirclePath4}`}/>
+                <div className={`${styles.cloudCircle} ${styles.cloudCirclePath5}`}/>
+                <div className={`${styles.cloudCircle} ${styles.cloudCirclePath6}`}/>
+            </div>
+            <Image className={styles.rainDrop} src={raindrop} height={30} width={30} alt='raindrop' onTransitionEnd={handleRaindropTransitionEnd}/>
+        </div>
+    )
 }
